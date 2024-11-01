666b9e9bda1bbfa99a0c2004966a75b7b915af32
Manan Sanson <81453560+MananSanson@users.noreply.github.com>
2024-11-02 03:51:38 +0530

    Update productActions.js

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -2,7 +2,7 @@
 import axios from "axios"
 import * as actionTypes from '../constants/productConstants';
 
-const URL= 'http://localhost:8000';
+const URL= "https://flipkart-clone-3.onrender.com";
 
 
 export const getProducts= ()=> async(dispatch)=>{
